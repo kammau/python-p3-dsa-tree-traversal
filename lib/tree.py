f92199723b85909698c0065d47ebc2fbb0569307
Kamryn Smith <programersign@gmail.com>
2023-07-04 12:44:23 -0700

    assignment complete

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,12 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+    while len(nodes_to_visit) > 0:
+      current_node = nodes_to_visit.pop()
+      if current_node['id'] == id:
+        return current_node
+      # if any children:
+      nodes_to_visit = nodes_to_visit + current_node['children']
+    # else:
+    return None
